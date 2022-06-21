69fdc48ba18165f5882a49ea3208f1c206800b60
Joussemar Borges <joussemar.borges@gmail.com>
2022-06-21 20:44:39 -0300

    Finalizando o exercício 3

---
+++ Fundamentos/bloco4_Introducao-a-JavaScript-e-LogicaDeProgramacao/4.1-Introdução-JavaScript/exercicio_3.js
@@ -0,0 +1,15 @@
+const a = 35;
+const b = 13;
+const c = 25;
+
+if (a > b && a > c){
+
+    console.log(a);
+}
+
+else if (b > a && b > c){
+    console.log(b);
+}
+else if (c > a && c > b){
+    console.log(c);
+}
