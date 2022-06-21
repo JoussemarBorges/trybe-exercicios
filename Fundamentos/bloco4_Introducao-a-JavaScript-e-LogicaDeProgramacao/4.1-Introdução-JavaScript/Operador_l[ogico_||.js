a7228d40085281db815442d2ed64b680d5504e26
Joussemar Borges <joussemar.borges@gmail.com>
2022-06-21 19:17:32 -0300

    Estruturando os arquivos do dia dentro da branch correspondente

---
+++ Fundamentos/bloco4_Introducao-a-JavaScript-e-LogicaDeProgramacao/4.1-Introdução-JavaScript/Operador_l[ogico_||.js
@@ -0,0 +1,8 @@
+let weekDay = "Quarta-feira"
+
+if (weekDay === "Segunda-feira" || weekDay === "Terça-feira" || weekDay === "Quarta-feira" || weekDay === "Quinta-feira" || weekDay === "Sexta-feira"){
+    console.log("Oba, mais um dia de aprendizado na Trybe >:D");
+}
+else if(weekDay === "Sábado" || weekDay === "Domingo"){
+    console.log("FINALMENTE, descanso merecido UwU");
+}
