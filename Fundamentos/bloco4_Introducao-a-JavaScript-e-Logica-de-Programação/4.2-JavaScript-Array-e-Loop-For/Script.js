6ebcac069269b1c2cc498387f399a8f72d785ada
Joussemar Borges <joussemar.borges@gmail.com>
2022-06-22 18:55:52 -0300

    Finalizando todos os exercícios do dia em um só arquivo

---
+++ Fundamentos/bloco4_Introducao-a-JavaScript-e-Logica-de-Programação/4.2-JavaScript-Array-e-Loop-For/Script.js
@@ -0,0 +1,13 @@
+let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+//console.log(tasksList.length, tasksList[tasksList.length -1]);
+// 3
+
+//tasksList.unshift('Fazer exercícios da Trybe');  // adiciona mais uma tarefa
+//console.log(tasksList);
+
+//tasksList.pop();  // remove a última tarefa
+//console.log(tasksList);
+
+let indexOfTask = tasksList.indexOf('Reunião');
+console.log(indexOfTask);
