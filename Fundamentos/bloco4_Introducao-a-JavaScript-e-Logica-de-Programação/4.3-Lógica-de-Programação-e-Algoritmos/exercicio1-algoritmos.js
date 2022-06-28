b499797173d40ff45f4def4b134aaa4f167b51c2
Joussemar Borges <joussemar.borges@gmail.com>
2022-06-28 09:51:26 -0300

    Teste

--- Fundamentos/bloco4_Introducao-a-JavaScript-e-Logica-de-Programação/4.3-Lógica-de-Programação-e-Algoritmos/exercicio1-algoritmos.js
+++ Fundamentos/bloco4_Introducao-a-JavaScript-e-Logica-de-Programação/4.3-Lógica-de-Programação-e-Algoritmos/exercicio1-algoritmos.js
@@ -1,10 +1,19 @@
-let priNum = 1;
-let num = 10;
+// let priNum = 1;
+// let num = 10;
 
-let fatorial = 1;
+// let fatorial = 1;
 
-for(index = 1; index <=10; index += 1){
-    fatorial = fatorial * index;
+// for(index = 1; index <=10; index += 1){
+//     fatorial = fatorial * index;
+// }
+
+// console.log(fatorial);
+
+
+function fatorial(priNum, num){
+    for(index = priNum; index <=num; index += 1){
+        fatorial = fatorial * index;
+    }
 }
 
-console.log(fatorial);
+console.log(fatorial(1, 10));
---
+++ Fundamentos/bloco4_Introducao-a-JavaScript-e-Logica-de-Programação/4.4-JavaScript-Objetos-e-Funções/Exemplo1_ForIn.js
@@ -0,0 +1,78 @@
+// let pizzas = {
+//     sabor: "palmito",
+//     preco: 39.90,
+//     bordaCatupiry: true
+// };
+
+// // for (let key in pizzas){
+
+// //     console.log(key);
+
+// // }
+
+// // for (let key in pizzas){
+
+// //     console.log(pizzas);
+
+// // }
+
+// // for (let key in pizzas){
+
+// //     console.log(pizzas["bordaCatupiry"]);
+
+// // }
+
+// // for (let key in pizzas){
+
+// //     console.log(pizzas[key]);
+
+// // }
+
+// let pizzasDoces = ['chocolate', 'banana', 'morango'];
+
+// for (let key in pizzasDoces){
+    
+//     console.log(key, pizzasDoces[key]);
+// }
+
+// let cars = ['Saab', 'Volvo', 'BMW'];
+
+// for (let index in cars){
+//     console.log(cars[index], index);
+// }
+
+// let cars = {
+//     type: 'Fiat',
+//     model: 500,
+//     color: 'white',
+// };
+
+// for(let index in cars){
+//     console.log(index, cars[index]);
+// }
+
+// let food = ['hamburgues', 'bife', 'acarajé'];
+
+// for(let value of food){
+//     console.log(value);
+// }
+
+// let names = {
+//     person1: "João",
+//     person2: "Maria",
+//     person3: "Jorge",
+// };
+
+// for (let index in names){
+//     console.log("Olá ", names[index]);
+// }
+
+let car = {
+    model1: "A3 Sedan",
+    manufacturer: "Audi",
+    year: 2020,
+};
+
+for (let key in car){
+    console.log(key, car[key]);
+}
