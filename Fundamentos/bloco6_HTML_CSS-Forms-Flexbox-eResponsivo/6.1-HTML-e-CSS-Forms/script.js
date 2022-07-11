79ef1e78c3102691ef71c7577d7f7e98e9382e37
Joussemar Borges <joussemar.borges@gmail.com>
2022-07-11 14:58:05 -0300

    Comitando os exercicios do dia.

--- Fundamentos/bloco6_HTML_CSS-Forms-Flexbox-eResponsivo/6.1-HTML-e-CSS-Forms/script.js
+++ Fundamentos/bloco6_HTML_CSS-Forms-Flexbox-eResponsivo/6.1-HTML-e-CSS-Forms/script.js
@@ -1,20 +1,56 @@
-// SELETORES
-const INPUT_TEXT = document.querySelector("#input-text");
-const INPUT_CHECKBOX = document.querySelector("#input-checkbox");
-const HREF_LINK = document.querySelector("#href");
+function clearFields() {
+const allInputs = document.querySelectorAll('input');
+const textArea = document.querySelector('#textarea-1');
+for (let index = 0; index < allInputs.length; index += 1) {
+ 
+    const userInput = allInputs[index];
+    userInput.value = '';
+    userInput.checked = false;
+}
+textArea.value = '';
+}
 
-HREF_LINK.addEventListener("click", (event) => {
-    event.prevenerDefault();
-});
+function enableButton() {
+    const submitBtn = document.querySelector('#submit-btn');
+    const agreement = document.querySelector('#checkbox2');
 
-INPUT_CHECKBOX.addEventListener("change", (event) => {
-    event.preventDefault();
-});
+    submitBtn.disabled = !agreement.checked;
+}
 
-INPUT_TEXT.addEventListener("keypress", (event) => {
-    const character = event.key;
+function textInputValidation () {
+    const email = document.querySelector('#email').value.length;
+    const invalidEmail = email < 10 || email > 50;
 
-    if (character !== "a") {
-        event.preventDefault();
+    const name = documento.querySelector('#nome').value.length;
+    const invalidName = name < 10 || name > 40;
+
+    const textArea1 = document.querySelector('#textarea-1').value.length;
+    const invalidTextArea = textArea1 > 500;
+
+    if (invalidEmail || invalidName || invalidTextArea) {
+        return false;
     }
-});
+    else {
+        return true;
+    }
+}
+
+
+window.onload = function() {
+    const submitBtn = document.querySelector('#submit-btn');
+    submitBtn.addEventListener('click', function(event) {
+        event.preventDefault();
+        const validation = textInputValidation();
+        if (validation === false) {
+            alert('Dados inválidos')
+        } else {
+            alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.');
+        }
+    });
+
+    const clearBtn = document.querySelector('#clear-btn');
+    clearBtn.addEventListener('click', clearFields);
+
+    const agreement = document.querySelector('#checkbox2');
+    agreement.addEventListener('change', enableButton);
+};
