4b687e6403521edfabb1062f9b4e59f3c4bf1d71
Joussemar Borges <joussemar.borges@gmail.com>
2022-07-17 23:38:26 -0300

    Finalizando o exercício 2

---
+++ Fundamentos/bloco7_Introducao-a-JavaScript-ES6-e-Testes-Unitarios/7.1-JavaScript-ES6-let-const,arrow-Functions-e-Testes-Unitarios/exercício2.js
@@ -0,0 +1,7 @@
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+// Seu código aqui.
+
+// console.log(oddsAndEvens); // será necessário alterar essa linha 😉
+
+console.log(`Os números ${oddsAndEvens.sort((a, b) => a - b)} se encontram ordenados de forma crescente!`);
